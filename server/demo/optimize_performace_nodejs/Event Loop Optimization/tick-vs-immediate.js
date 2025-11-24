c6977001edc1f670911f0dd27c17816c3bf58fdf
Sayed Abdul <sakarim9124@gmail.com>
2025-11-24 13:30:33 +0530

    updated

--- server/demo/optimize_performace_nodejs/Event Loop Optimization/tick-vs-immediate.js
+++ server/demo/optimize_performace_nodejs/Event Loop Optimization/tick-vs-immediate.js
@@ -1,11 +1,14 @@
 console.log("1 - Start");
 
-setImmediate(() => {
-  console.log("2 - setImmediate");
-});
-
 process.nextTick(() => {
   console.log("3 - nextTick");
 });
 
+setImmediate(() => {
+  console.log("2 - setImmediate");
+});
+
+setTimeout(() => {
+  console.log("5 - Timer executed");
+}, 0);
 console.log("4 - End");
